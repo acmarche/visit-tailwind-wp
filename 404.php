2186774fb7f9b94689ebd5475b2631447a071ab8
jfsenechal <jfsenechal@gmail.com>
2022-11-10 13:44:20 +0100

    debug language

--- 404.php
+++ 404.php
@@ -2,10 +2,19 @@
 
 namespace VisitMarche\ThemeTail;
 
+use VisitMarche\ThemeTail\Inc\Menu;
+use VisitMarche\ThemeTail\Lib\LocaleHelper;
 use VisitMarche\ThemeTail\Lib\Twig;
 
 get_header();
 
+$language = LocaleHelper::getSelectedLanguage();
+dump(111);
+dump($language);
+dump(111);
+$menu = new Menu();
+$items = $menu->getMenuTop();
+dump($items);
 Twig::rendPage(
     '@VisitTail/errors/404.html.twig',
     [
