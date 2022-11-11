f07073df4d9f577463d11b0686a528338c749cc4
jfsenechal <jfsenechal@gmail.com>
2022-11-11 20:10:09 +0100

    test gpx

--- 404.php
+++ 404.php
@@ -3,19 +3,36 @@
 namespace VisitMarche\ThemeTail;
 
 use VisitMarche\ThemeTail\Inc\Menu;
+use VisitMarche\ThemeTail\Lib\GpxViewer;
 use VisitMarche\ThemeTail\Lib\LocaleHelper;
 use VisitMarche\ThemeTail\Lib\Twig;
 
 get_header();
 
 $language = LocaleHelper::getSelectedLanguage();
-dump(111);
-dump($language);
-dump(111);
+//dump($language);
 $menu = new Menu();
 $items = $menu->getMenuTop();
-dump(ICL_LANGUAGE_CODE);
-dump($items);
+//dump(ICL_LANGUAGE_CODE);
+//dump($items);
+
+$filepath = '/wp-content/uploads/gpx/non-classifiee/Cirkwi-Marche-en-Famenne_-_Circuit_VTT_Vert.gpx';
+
+$pgwViewer = new GpxViewer();
+$pgwViewer->renderWithPlugin($filepath);
+
+$gpx = gpx_view(array(
+        'src' => $filepath,
+        'title' => 'Cirkwi-Marche-en-Famenne_-_Circuit_VTT_Vert.gpx',
+        'color' => '#FF0000',
+        'width' => '450px',
+        'distance_unit'=>'km',
+        'download_button' => true,
+    )
+);
+
+echo $gpx;
+
 Twig::rendPage(
     '@VisitTail/errors/404.html.twig',
     [
@@ -24,6 +41,7 @@ Twig::rendPage(
         'url' => '/',
         'latitude' => '5.342961',
         'longitude' => '50.226484',
+        'gpx'=>$gpx
     ]
 );
 get_footer();
