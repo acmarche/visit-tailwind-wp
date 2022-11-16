569e425fadd1ced81a9e7024398d83ea76ff333d
jfsenechal <jfsenechal@gmail.com>
2022-11-16 16:57:57 +0100

    clean

--- 404.php
+++ 404.php
@@ -9,6 +9,29 @@ use VisitMarche\ThemeTail\Lib\Twig;
 
 get_header();
 
+$language = LocaleHelper::getSelectedLanguage();
+//dump($language);
+$menu = new Menu();
+$items = $menu->getMenuTop();
+//dump(ICL_LANGUAGE_CODE);
+//dump($items);
+
+$filepath = '/wp-content/uploads/gpx/non-classifiee/Cirkwi-Marche-en-Famenne_-_Circuit_VTT_Vert.gpx';
+
+$pgwViewer = new GpxViewer();
+$pgwViewer->renderWithPlugin($filepath);
+
+$gpx = gpx_view(array(
+        'src' => $filepath,
+        'title' => 'Cirkwi-Marche-en-Famenne_-_Circuit_VTT_Vert.gpx',
+        'color' => '#FF0000',
+        'width' => '450px',
+        'distance_unit'=>'km',
+        'download_button' => true,
+    )
+);
+
+echo $gpx;
 
 Twig::rendPage(
     '@VisitTail/errors/404.html.twig',
@@ -18,7 +41,7 @@ Twig::rendPage(
         'url' => '/',
         'latitude' => '5.342961',
         'longitude' => '50.226484',
-
+        'gpx'=>$gpx
     ]
 );
 get_footer();
