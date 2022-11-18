1b8c1777869f8cd2219909fa9d57cc9fb04ffe60
jfsenechal <jfsenechal@gmail.com>
2022-11-18 14:49:16 +0100

    add locale helper sf

--- Lib/LocaleHelper.php
+++ Lib/LocaleHelper.php
@@ -2,12 +2,21 @@
 
 namespace VisitMarche\ThemeTail\Lib;
 
+use AcMarche\Pivot\DependencyInjection\PivotContainer;
+use AcMarche\Pivot\Utils\LocalSwitcherPivot;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
 use Symfony\Component\Translation\Translator;
 use Symfony\Component\Translation\TranslatorBagInterface;
 
 class LocaleHelper
 {
+    private LocalSwitcherPivot $localeSwitcher;
+
+    public function __construct()
+    {
+        $this->localeSwitcher = PivotContainer::getLocalSwitcherPivot();
+    }
+
     public static function getSelectedLanguage(): string
     {
         if (defined(ICL_LANGUAGE_CODE)) {
@@ -15,6 +24,16 @@ class LocaleHelper
         }
 
         return 'fr';
+    }
+
+    public function getCurrentLanguageSf(): string
+    {
+        return $this->localeSwitcher->getLocale();
+    }
+
+    public function setCurrentLanguageSf(string $locale): void
+    {
+        $this->localeSwitcher->setLocale($locale);
     }
 
     public static function iniTranslator(): TranslatorBagInterface
