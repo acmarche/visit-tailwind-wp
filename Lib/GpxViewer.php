a77cb2b924b0057ab29aca0ce723bb6de96afc57
jfsenechal <jfsenechal@gmail.com>
2022-11-18 20:48:03 +0100

    gpx viewer ok

--- Lib/GpxViewer.php
+++ Lib/GpxViewer.php
@@ -4,12 +4,23 @@ namespace VisitMarche\ThemeTail\Lib;
 
 use AcMarche\Pivot\Entities\Specification\Gpx;
 use Exception;
+use phpGPX\Models\Point;
+use phpGPX\phpGPX;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpClient\Exception\ClientException;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use WP_Post;
+
 
 class GpxViewer
 {
+    private string $folder_gpx = 'var/gpx/';
+
     public function render(array $args): string
     {
         $fileName = $args['file'] ?? null;
@@ -78,25 +89,23 @@ class GpxViewer
         );
     }
 
-    public function renderWithPlugin(string $codeCgt, string $filePath): string
+    public function renderWithPlugin(string $codeCgt, string $url): string
     {
-        // $filePath = '/wp-content/uploads/gpx/non-classifiee/Cirkwi-Marche-en-Famenne_-_Circuit_VTT_Vert.gpx';
-        //ko elevation
-        //$filePath = 'https://visit.marche.be/output/ANX-01-09DV-003E.gpx';
-        //cleaning elevation ok
-        //$filePath = 'https://visit.marche.be/wp-content/uploads/gpx/non-classifiee/Cirkwi-Marche-en-Famenne_-_Circuit_VTT_Vert.gpx';
-
-        $this->writeTmpFile2($codeCgt, $filePath);
-        $url = RouterPivot::getUrlSite().'/var/cache/gpx/'.$codeCgt.'.xml';
+        $fileName = $codeCgt.'xml';
+        $filePath = ABSPATH.$this->folder_gpx.$fileName;
+        if (!$this->writeTmpFile2($filePath, $url)) {
+            $this->elevation($filePath);
+        }
+        $urlLocal = RouterPivot::getUrlSite().'/'.$this->folder_gpx.$fileName;
         $gpx = gpx_view(array(
-                'src' => $url,
+                'src' => $urlLocal,
                 'title' => 'Gpx',
                 'color' => '#00ff00',
                 'width' => '5',
                 'distance_unit' => 'km',
                 "height_unit" => "m",
                 "step_min" => "10",
-                "icon_url" => "https://visit.marche.be/wp-content/plugins/gpx-viewer/images/",
+                "icon_url" => RouterPivot::getUrlSite()."/wp-content/plugins/gpx-viewer/images/",
                 'download_button' => true,
             )
         );
@@ -104,21 +113,91 @@ class GpxViewer
         return $gpx;
     }
 
-    public function writeTmpFile2(string $fileName, string $url): string
+    public function elevation(string $pathName)
     {
-        $pathName = ABSPATH.'var/cache/gpx/'.$fileName.'.xml';
-        if (is_readable($fileName)) {
-            return $fileName;
+        $gpx = new phpGPX();
+        $file = $gpx->load($pathName);
+        $locations = [];
+        foreach ($file->tracks as $track) {
+            // Statistics for whole track
+            $stats = $track->stats;
+            foreach ($track->segments as $segment) {
+                // Statistics for segment of track
+                foreach ($segment->getPoints() as $point) {
+                    $locations[] = ['latitude' => $point->latitude, 'longitude' => $point->longitude];
+                }
+            }
+        }
+        $elevationsString = $this->requestElevation($locations);
+        $elevations = json_decode($elevationsString);
+
+        foreach ($file->tracks as $track) {
+            // Statistics for whole track
+            $stats = $track->stats;
+            foreach ($track->segments as $segment) {
+                // Statistics for segment of track
+                foreach ($segment->getPoints() as $point) {
+                    if (!$this->findSegment($point, $elevations->results)) {
+                        dd($point);
+                    }
+                }
+            }
+        }
+        $file->save($pathName, phpGPX::XML_FORMAT);
+    }
+
+    private function findSegment(Point $point, array $elevations): bool
+    {
+        foreach ($elevations as $elevation) {
+            if ($elevation->latitude === $point->latitude && $elevation->longitude == $point->longitude) {
+                $point->elevation = $elevation->elevation;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function requestElevation(array $locations): string
+    {
+        $url = 'https://api.open-elevation.com/api/v1/lookup';
+        $headers = [
+            'headers' => ['Accept' => 'application/json', 'Content-Type' => 'application/json'],
+        ];
+
+        $this->httpClient = HttpClient::create($headers);
+        $data = json_encode(['locations' => $locations]);
+
+        try {
+            $response = $this->httpClient->request(
+                'POST',
+                $url, [
+                    'body' => $data,
+                ]
+            );
+
+            $data_raw = $response->getContent();
+
+            return $data_raw;
+        } catch (ClientException|ClientExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface|TransportExceptionInterface $exception) {
+            throw  new Exception($exception->getMessage(), $exception->getCode(), $exception);
+        }
+    }
+
+    public function writeTmpFile2(string $filePath, string $url): bool
+    {
+        if (is_readable($filePath)) {
+            return true;
         }
         try {
             $filesystem = new Filesystem();
-            //$filesystem->dumpFile(sys_get_temp_dir().'/'.'file.gpx', file_get_contents($gpx->url));
-            $filesystem->dumpFile($pathName, file_get_contents($url));
+            $filesystem->dumpFile($filePath, file_get_contents($url));
         } catch (IOExceptionInterface $exception) {
             echo "An error occurred while creating your directory at ".$exception->getPath();
         }
 
-        return $fileName;
+        return false;
     }
 
     public function writeTmpFile(Gpx $gpx): string
