f3fca2c188d6cdac15e6ce9c26c36f128db70810
jfsenechal <jfsenechal@gmail.com>
2022-11-20 12:42:35 +0100

    add error page

--- error.php
+++ error.php
@@ -1,18 +1,23 @@
 <?php
+
 namespace VisitMarche\ThemeTail;
 
 use VisitMarche\ThemeTail\Lib\Mailer;
+use VisitMarche\ThemeTail\Lib\RouterPivot;
+use VisitMarche\ThemeTail\Lib\Twig;
 
-?>
-    <div class="mt-6">
-        <h1>Oops! An Error Occurred2222</h1>
-        <h2>The server returned a "<?= $statusCode; ?> <?= $statusText; ?>".</h2>
+//$statusCode;  $statusText;
+Twig::rendPage(
+    '@VisitTail/errors/500.html.twig',
+    [
+        'excerpt' => null,
+        'image' => get_template_directory_uri().'/assets/images/error500.jpg',
+        'urlBack' => '/',
+        'categoryName' => 'Accueil',
+        'nameBack' => 'Acceuil',
+    ]
+);
 
-        <p>
-            Something is broken. Please let us know what you were doing when this error occurred.
-            We will fix it as soon as possible. Sorry for any inconvenience caused.
-        </p>
-    </div>
-    <?php
-Mailer::sendError('error visit', "page x".$statusCode);
+get_footer();
+Mailer::sendError('error visit', "page ".RouterPivot::getCurrentUrl());
 ?>
