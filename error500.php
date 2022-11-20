efc845caaf665186b9d9f8e62480663cdb2626e1
jfsenechal <jfsenechal@gmail.com>
2022-11-20 18:35:33 +0100

    clean

--- error500.php
+++ error500.php
@@ -2,15 +2,16 @@
 
 namespace VisitMarche\ThemeTail;
 
-use Symfony\Component\ErrorHandler\Debug;
+//use Symfony\Component\ErrorHandler\Debug;
 use VisitMarche\ThemeTail\Lib\Mailer;
 use VisitMarche\ThemeTail\Lib\RouterPivot;
 use VisitMarche\ThemeTail\Lib\Twig;
 
 //$statusCode;  $statusText;
-Debug::enable();
-Twig::rend500Page();
+//Debug::enable();
 
+//get_header();
+Twig::rend500Page();
 get_footer();
 
 try {
