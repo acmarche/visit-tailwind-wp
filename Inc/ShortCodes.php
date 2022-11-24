c6d942cae84f8651011e7da74a179988d377d4fe
jfsenechal <jfsenechal@gmail.com>
2022-11-24 14:50:09 +0100

    active short code gpx viewer

--- Inc/ShortCodes.php
+++ Inc/ShortCodes.php
@@ -2,21 +2,25 @@
 
 namespace VisitMarche\ThemeTail\Inc;
 
+use VisitMarche\ThemeTail\Lib\GpxViewer;
+
 class ShortCodes
 {
     public function __construct()
     {
-        // add_action('init', fn() => $this->registerShortcodes());
+        add_action('init', fn() => $this->registerShortcodes());
     }
 
     public function registerShortcodes(): void
     {
-        // add_shortcode('gpx_viewer', fn($args): string => (new self())->doSomething($args));
+        add_shortcode('gpx_viewer', fn($args): string => (new self())->gpxViewer($args));
     }
 
-    public function doSomething($args)
+    public function gpxViewer($args): string
     {
+        $gpxViewer = new GpxViewer();
 
+        return $gpxViewer->render($args);
     }
 
 }
