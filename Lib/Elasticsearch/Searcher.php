46caccc1b5df203fa04fb774a4ad58773c6a5981
jfsenechal <jfsenechal@gmail.com>
2022-11-28 10:59:08 +0100

    error

--- Lib/Elasticsearch/Searcher.php
+++ Lib/Elasticsearch/Searcher.php
@@ -4,6 +4,14 @@ namespace VisitMarche\ThemeTail\Lib\Elasticsearch;
 
 use Elastica\Exception\InvalidException;
 use Elastica\ResultSet;
+use Symfony\Component\HttpClient\Exception\ClientException;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use VisitMarche\ThemeTail\Lib\Mailer;
 use WP_Query;
 
 /**
@@ -12,11 +20,34 @@ use WP_Query;
  */
 class Searcher
 {
+    private HttpClientInterface $httpClient;
+
+    public function __construct()
+    {
+        $this->httpClient = HttpClient::create();
+
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function searchFromWww(string $keyword): bool|string
     {
-        return file_get_contents(
-            'https://www.marche.be/visit-elasticsearch/search.php?keyword='.urlencode($keyword)
-        );
+        $url = 'https://www.marche.be/visit-elasticsearch/search.php?keyword='.urlencode($keyword);
+
+        try {
+            $response = $this->httpClient->request(
+                'GET',
+                $url, [
+
+                ]
+            );
+
+            return $response->getContent();
+        } catch (ClientException|ClientExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface|TransportExceptionInterface $exception) {
+            Mailer::sendError('search', 'erreur '.$exception->getMessage());
+            throw  new \Exception($exception->getMessage(), $exception->getCode(), $exception);
+        }
     }
 
     /**
@@ -28,26 +59,31 @@ class Searcher
      */
     public function searchRecommandations(WP_Query $wp_query): array
     {
-        $hits = [];
-
         $queries = $wp_query->query;
         $queryString = implode(' ', $queries);
         $queryString = preg_replace('#-#', ' ', $queryString);
         $queryString = preg_replace('#/#', ' ', $queryString);
         $queryString = strip_tags($queryString);
         if ('' !== $queryString) {
-            $results = $this->searchFromWww($queryString);
-            $hits = json_decode($results, null, 512, JSON_THROW_ON_ERROR);
+            try {
+                $results = $this->searchFromWww($queryString);
+                $hits = json_decode($results, null, 512, JSON_THROW_ON_ERROR);
+            } catch (\Exception $e) {
+                return [];
+            }
+
+            return array_map(
+                function ($hit) {
+                    $hit->title = $hit->name;
+                    $hit->tags = [];
+
+                    return $hit;
+                },
+                $hits
+            );
         }
 
-        return array_map(
-            function ($hit) {
-                $hit->title = $hit->name;
-                $hit->tags = [];
+        return [];
 
-                return $hit;
-            },
-            $hits
-        );
     }
 }
