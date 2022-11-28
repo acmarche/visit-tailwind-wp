e59c2a7c17a0197d5222621c5fe62662efe73997
jfsenechal <jfsenechal@gmail.com>
2022-11-28 10:47:13 +0100

    empty

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
 namespace VisitMarche\ThemeTail;
 
+use VisitMarche\ThemeTail\Lib\Twig;
+
 get_header();
+
+Twig::rendPage(
+    '@VisitTail/empty.html.twig',
+    [
+        'name' => 'Page index',
+        'post' => null,
+        'excerpt' => '',
+        'tags' => [],
+        'image' => null,
+        'icone' => null,
+        'recommandations' => [],
+        'bgCat' => '',
+        'urlBack' => '',
+        'categoryName' => 'name',
+        'nameBack' => '',
+        'content' => '',
+    ]
+);
 
 get_footer();
