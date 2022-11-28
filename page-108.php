660679e5768c026a9ffef32ff8f60585c007de60
jfsenechal <jfsenechal@gmail.com>
2022-11-28 10:27:52 +0100

    fix page

--- page-108.php
+++ page-108.php
@@ -2,26 +2,19 @@
 
 namespace VisitMarche\ThemeTail;
 
-use VisitMarche\ThemeTail\Lib\LocaleHelper;
 use VisitMarche\ThemeTail\Lib\PostUtils;
 use VisitMarche\ThemeTail\Lib\Twig;
-use VisitMarche\ThemeTail\Lib\WpRepository;
 
 get_header();
 
 global $post;
 
-$wpRepository = new WpRepository();
-
 $image = PostUtils::getImage($post);
-$tags = $wpRepository->tagsOfPost($post->ID);
+$tags = PostUtils::tagsPost($post);
 
 $content = get_the_content(null, null, $post);
 $content = apply_filters('the_content', $content);
 $content = str_replace(']]>', ']]&gt;', $content);
-
-$translator = LocaleHelper::iniTranslator();
-$nameBack = $translator->trans('menu.home');
 
 Twig::rendPage(
     '@VisitTail/page_select_language.html.twig',
