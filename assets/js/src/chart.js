7542d975d7a4440e7d39a577a48515b01a307cb8
jfsenechal <jfsenechal@gmail.com>
2023-01-09 20:19:26 +0100

    position cursor cool

--- assets/js/src/chart.js
+++ assets/js/src/chart.js
@@ -13,8 +13,7 @@ export const createChart = (marker) => {
         let firstElement = data[i];
         let secondElement = data[i + 1];
         let result = distanceInMeter(firstElement.latitude, firstElement.longitude, secondElement.latitude, secondElement.longitude);
-
-        distances.push(result + ' metres');
+        distances.push(result);
     }
 
     const metres = [];
@@ -32,10 +31,59 @@ export const createChart = (marker) => {
         elevations.push(item.elevation);
     });
 
+    const footer = (tooltipItems) => {
+        return 'Sum: ';
+    };
+
+    const options = {
+        responsive: false,
+        plugins: {
+            legend: {
+                display: false
+            },
+            tooltip: {
+                mode: 'index',
+                intersect: false,
+                callbacks: {
+                    footer: footer,
+                }
+            },
+            title: {
+                display: true,
+                text: 'Elevations'
+            },
+        },
+        hover: {
+            mode: 'index',//point ?
+        },
+        onHover: (e) => {
+            const lm = (myChart.getElementsAtEventForMode(e, 'index', {intersec: false}, true))
+            const index = lm[0].index
+            const coordinates = data[index]
+            movePoint(marker, coordinates)
+        },
+        // For instance you can format Y axis
+        scales: {
+            y: {
+                beginAtZero: true,
+                min: 200,
+                max: 350
+            },
+            x: {
+                ticks: {
+                    callback: function(value)  {
+                        //     console.log(value)
+                        let formattedValue = `${parseInt(value) / 10} km`
+                        //       console.log(formattedValue)
+                        return formattedValue
+                    }
+                }
+            }
+        }
+    }
+
     const myChart = new Chart(ctx, {
         type: 'line',
-        height: '150',
-        width: '150',
         data: {
             labels: metres.map(row => Math.floor(row)),
             datasets: [{
@@ -48,75 +96,21 @@ export const createChart = (marker) => {
                 fill: 'start'
             }]
         },
-        options: {
-            onClick: function (event, elements) {
-                if (elements.length > 0) {
-                    // To get the clicked element
-                    console.log(event)
-                    //      const clickedElement = this.getElementAtEvent(event);
-
-                    // To get the group id of the clicked element
-                    //    const groupIndex = clickedElement[0]._index;
-
-                    // To get the id of the clicked element with in the group
-                    //  const barIndex = clickedElement[0]._datasetIndex;
-                }
-            },
-            responsive: true,
-            plugins: {
-                legend: {
-                    display: false
-                },
-                tooltip: {
-                    mode: 'index',
-                    intersect: false
-                },
-                title: {
-                    display: true,
-                    text: 'Elevations'
-                },
-            },
-            hover: {
-                mode: 'index',//point ?
-            },
-            onHover: (e) => {
-                //const canvasPosition = getRelativePosition(e, myChart);
-                const lm = (myChart.getElementsAtEventForMode(e, 'index', {intersec: false}, true))
-               // console.log(lm)
-                const index = lm[0].index
-              //  console.log(index)
-                const coordinates = data[index]
-              //  console.log(coordinates)
-                movePoint(marker, coordinates)
-            },
-            // For instance you can format Y axis
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    min: 100,
-                    max: 350
-                },
-                x: {
-                    ticks: {
-                        callback: value => `${parseInt(value) / 10} km`
-                    }
-                }
-            }
-        }
+        options: options
     });
 
 }
 
 function distanceInMeter(lat1, lon1, lat2, lon2) {
     const R = 6371e3; // metres
-    const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
-    const φ2 = lat2 * Math.PI / 180;
-    const Δφ = (lat2 - lat1) * Math.PI / 180;
-    const Δλ = (lon2 - lon1) * Math.PI / 180;
+    const pi1 = lat1 * Math.PI / 180; // φ, λ in radians
+    const pi2 = lat2 * Math.PI / 180;
+    const pi3 = (lat2 - lat1) * Math.PI / 180;
+    const pi4 = (lon2 - lon1) * Math.PI / 180;
 
-    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-        Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const a = Math.sin(pi3 / 2) * Math.sin(pi3 / 2) +
+        Math.cos(pi1) * Math.cos(pi2) *
+        Math.sin(pi4 / 2) * Math.sin(pi4 / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     const d = R * c; // in metres
