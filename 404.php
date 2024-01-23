978093f8c0a752f8e7d087f8786bb3fd3de14fb0
jfsenechal <jfsenechal@gmail.com>
2024-01-23 09:03:27 +0100

    cleaning

--- 404.php
+++ 404.php
@@ -2,18 +2,8 @@
 
 namespace VisitMarche\ThemeTail;
 
-use VisitMarche\ThemeTail\Inc\Menu;
-use VisitMarche\ThemeTail\Lib\LocaleHelper;
-use VisitMarche\ThemeTail\Lib\RouterPivot;
 use VisitMarche\ThemeTail\Lib\Twig;
 
 get_header();
-
-$language = LocaleHelper::getSelectedLanguage();
-//dump($language);
-$menu = new Menu();
-$items = $menu->getMenuTop();
-//dump(ICL_LANGUAGE_CODE);
-//dump($items);
 Twig::rend404Page();
 get_footer();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 VistMarche Theme Wp
 ===
+
+npx tailwindcss -i tailwind.css -o assets/visit.css --watch
+
