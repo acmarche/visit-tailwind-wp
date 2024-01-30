e8d5aa9c4ec042541622f6c9deae51ed3b340ad8
jfsenechal <jfsenechal@gmail.com>
2024-01-30 13:21:34 +0100

    add sync codes cgt

--- Lib/SyncPivot.php
+++ Lib/SyncPivot.php
@@ -4,37 +4,30 @@ namespace VisitMarche\ThemeTail\Lib;
 
 class SyncPivot
 {
-    /**
-     * @return array|\WP_Term[]
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     */
-    public function categoriesWithFiltre(): array
-    {
-        $categories = [];
-        $wpRepository = new WpRepository();
-        foreach ($wpRepository->getCategoriesFromWp() as $category) {
-            $filtres = WpFilterRepository::getMetaPivotTypesOffre($category->term_id);
-            if (count($filtres) > 0) {
-                $categories[] = $category;
-            }
-        }
-
-        return $categories;
-    }
-
     public function syncUrns()
     {
-        $languages = ['en','nl'];
-        foreach ($this->categoriesWithFiltre() as $category) {
+        $languages = ['en', 'nl'];
+        $wpRepository = new WpRepository();
+        foreach ($wpRepository->getCategoriesFromWp() as $category) {
             $filtresFr = WpFilterRepository::getMetaPivotTypesOffre($category->term_id);
+            $codesCgtFr = WpFilterRepository::getMetaPivotCodesCgtOffres($category->term_id);
             foreach ($languages as $language) {
                 $categoryId = apply_filters('wpml_object_id', $category->term_id, 'category', true, $language);
                 if ($categoryId) {
                     $categoryLng = get_category($categoryId);
-                    $filtresEn = WpFilterRepository::getMetaPivotTypesOffre($categoryLng->term_id);
-                    $diff = array_diff(array_column($filtresFr, 'urn'), (array_column($filtresEn, 'urn')));
-                    if (count($diff) > 0) {
-                        update_term_meta($categoryLng->term_id, WpRepository::PIVOT_REFRUBRIQUE, $filtresFr);
+                    if (count($filtresFr) > 0) {
+                        $filtresLng = WpFilterRepository::getMetaPivotTypesOffre($categoryLng->term_id);
+                        $diff = array_diff(array_column($filtresFr, 'urn'), (array_column($filtresLng, 'urn')));
+                        if (count($diff) > 0) {
+                            update_term_meta($categoryLng->term_id, WpRepository::PIVOT_REFRUBRIQUE, $filtresFr);
+                        }
+                    }
+                    if (count($codesCgtFr) > 0) {
+                        $codesCgtLng = WpFilterRepository::getMetaPivotCodesCgtOffres($categoryLng->term_id);
+                        $diff = array_diff($codesCgtFr, $codesCgtLng);
+                        if (count($diff) > 0) {
+                            update_term_meta($categoryLng->term_id, WpRepository::PIVOT_REFOFFERS, $filtresFr);
+                        }
                     }
                 }
             }
