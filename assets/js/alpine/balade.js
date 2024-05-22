de8197104be8647f22f9ef7aebe35c885590f1be
jfsenechal <jfsenechal@gmail.com>
2024-05-22 16:15:10 +0200

    add map walks

--- assets/js/alpine/balade.js
+++ assets/js/alpine/balade.js
@@ -1,31 +1,27 @@
-/*
-import Alpine from '/node_modules/alpinejs/dist/module.esm.js';
-//import L from '/node_modules/leaflet/src/Leaflet.js';
-import {Map} from "https://unpkg.com/leaflet@1.7.1/src/map";
-import "https://unpkg.com/leaflet@1.7.1/src/layer/vector/Renderer.getRenderer";
-import "https://unpkg.com/leaflet@1.7.1/src/control/Control.Attribution";
-import "https://unpkg.com/leaflet@1.7.1/src/control/Control.Zoom";
-*/
-
 document.addEventListener('alpine:init', () => {
     Alpine.data('balades', () => ({
+        map: null,
         isOpen: false,
         menuSelected: 'map',
         tabOpen: '',
         menuOpen: false,
         leafletLoaded: false,
-        filtersSelected: {type: null, localite: null},
+        polyline: null,
+        markers:null,
+        walksCount: 0,
+        filtersSelected: {type: 11, localite: null},
+        codeCgtSelected: null,
+        previewOpen: false,
         toggle() {
             this.isOpen = !this.isOpen
             this.$refs.leaflet.innerHTML = 'Loading...'
-            //  this.loadLeaflet()
-        },
-        jf() {
-            console.log('jf')
-
         },
         init() {
+            this.$watch('filtersSelected', value => this.addMarkersGrouped())
             this.loadLeaflet()
+        },
+        initialized() {
+            console.log('initialized')
         },
         manageFilters(name, value, event) {
             if (!event.target.checked) {
@@ -43,7 +39,6 @@ document.addEventListener('alpine:init', () => {
                     this.filtersSelected.type = value
                 }
             }
-            console.log(this.filtersSelected)
         },
         toggleCollapsation(id) {
 
@@ -67,20 +62,84 @@ document.addEventListener('alpine:init', () => {
             if (this.leafletLoaded) {
                 return;
             }
-            console.log('leafletLoaded' + this.leafletLoaded)
             this.leafletLoaded = true
             const center = [50.217845, 5.331049]
             const zoom = 13
-            //const map = new Map("map").setView([51.505, -0.09], 13);
-            const mapDiv = document.getElementById('walks_map');
-            const map = L.map('walks_map').setView(center, zoom)
+            this.map = L.map('walks_map').setView(center, zoom)
 
             L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
                 minZoom: 1,
                 maxZoom: 20
             })
-                .addTo(map)
+                .addTo(this.map)
+            this.markers = L.markerClusterGroup();
+            this.addMarkersGrouped()
+        },
+        addMarkersGrouped() {
+            this.markers.clearLayers();
+            let iconSize = [25, 41]
+            let allWalks = JSON.parse(document.getElementById('maincarto').dataset.markers)
+            let data = allWalks.filter(this.checkFiltered(this.filtersSelected))
+            this.walksCount = data.length
+            data.forEach((offer) => {
+                const marker = L.marker(new L.LatLng(offer.address.latitude, offer.address.longitude), {
+                    title: offer.nom,
+                    icon: new L.Icon({iconUrl: this.iconMarker(offer), iconSize: iconSize})
+                });
+                marker.addEventListener('mouseover', () => {
+                    this.drawPolyline(offer)
+                });
+                marker.addEventListener('mouseout', () => {
+                    this.removePolyline()
+                });
+                marker.addEventListener('click', () => {
+                    this.walkPreview(offer.codeCgt)
+                });
+                this.markers.addLayer(marker);
+            })
+            this.map.addLayer(this.markers);
+        },
+        checkFiltered(filtersSelected) {
+            return function (offer) {
+                if (filtersSelected.localite === null && filtersSelected.type === 11) {
+                    return true
+                }
+                if (filtersSelected.localite) {
+                    if (offer.localite == filtersSelected.localite) {
+                        return true
+                    }
+                }
+                if (filtersSelected.type) {
+                    if (offer.type == filtersSelected.type) {
+                        return true
+                    }
+                }
+                return false
+            }
+        },
+        walkPreview(codeCgtSelected) {
+            this.codeCgtSelected = codeCgtSelected
+            this.previewOpen = true
+            //   scrollUp()
+        },
+        drawPolyline(offer) {
+            if (offer.locations.length > 0) {
+                const myStyle = {
+                    "color": "#487F89FF",
+                    "weight": 5,
+                    "opacity": 1
+                };
+                this.polyline = L.polyline(offer.locations, myStyle).addTo(map)
+            }
+        },
+        removePolyline() {
+            if (this.polyline != null) {
+                this.polyline.remove()
+            }
+        },
+        iconMarker(offer) {
+            return `/wp-content/themes/visittail/assets/images/map/marker-icon.png`
         }
     }));
 });
